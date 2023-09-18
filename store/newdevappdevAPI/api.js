eb619e36acc4254cec9696169a54f9f6461dc871
Robert So <robert.so@crowdbotics.com>
2023-09-18 14:20:03 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/newdevappdevAPI/api.js
+++ store/newdevappdevAPI/api.js
@@ -1,61 +1,84 @@
-import axios from "axios"
+import axios from "axios";
 const newdevappdevAPI = axios.create({
   baseURL: "https://new-dev-app-dev-114593.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
   return newdevappdevAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_login_create(payload) {
-  return newdevappdevAPI.post(`/api/v1/login/`, payload)
+  return newdevappdevAPI.post(`/api/v1/login/`, payload);
 }
+
 function api_v1_signup_create(payload) {
-  return newdevappdevAPI.post(`/api/v1/signup/`, payload)
+  return newdevappdevAPI.post(`/api/v1/signup/`, payload);
 }
+
 function modules_camera_photos_user_list(payload) {
-  return newdevappdevAPI.get(`/modules/camera/photos/user/`)
+  return newdevappdevAPI.get(`/modules/camera/photos/user/`);
 }
+
 function modules_camera_photos_user_retrieve(payload) {
-  return newdevappdevAPI.get(`/modules/camera/photos/user/${payload.id}/`)
+  return newdevappdevAPI.get(`/modules/camera/photos/user/${payload.id}/`);
 }
+
 function modules_camera_upload_image_create(payload) {
-  return newdevappdevAPI.post(`/modules/camera/upload_image/`)
+  return newdevappdevAPI.post(`/modules/camera/upload_image/`);
 }
+
 function rest_auth_login_create(payload) {
-  return newdevappdevAPI.post(`/rest-auth/login/`, payload)
+  return newdevappdevAPI.post(`/rest-auth/login/`, payload);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return newdevappdevAPI.get(`/rest-auth/logout/`)
+  return newdevappdevAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return newdevappdevAPI.post(`/rest-auth/logout/`)
+  return newdevappdevAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return newdevappdevAPI.post(`/rest-auth/password/change/`, payload)
+  return newdevappdevAPI.post(`/rest-auth/password/change/`, payload);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return newdevappdevAPI.post(`/rest-auth/password/reset/`, payload)
+  return newdevappdevAPI.post(`/rest-auth/password/reset/`, payload);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return newdevappdevAPI.post(`/rest-auth/password/reset/confirm/`, payload)
+  return newdevappdevAPI.post(`/rest-auth/password/reset/confirm/`, payload);
 }
+
 function rest_auth_registration_create(payload) {
-  return newdevappdevAPI.post(`/rest-auth/registration/`, payload)
+  return newdevappdevAPI.post(`/rest-auth/registration/`, payload);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return newdevappdevAPI.post(`/rest-auth/registration/verify-email/`, payload)
+  return newdevappdevAPI.post(`/rest-auth/registration/verify-email/`, payload);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return newdevappdevAPI.get(`/rest-auth/user/`)
+  return newdevappdevAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return newdevappdevAPI.put(`/rest-auth/user/`, payload)
+  return newdevappdevAPI.put(`/rest-auth/user/`, payload);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return newdevappdevAPI.patch(`/rest-auth/user/`, payload)
+  return newdevappdevAPI.patch(`/rest-auth/user/`, payload);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -74,4 +97,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
